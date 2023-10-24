b2dd2f17444f1ab831e979fefeeff0b94889bbea
Abiyyu Galih Ghifari <92355182+Abigalih@users.noreply.github.com>
2023-10-24 12:56:26 +0700

    dynamic card
    masih belom bisa fotonya

--- polyminal/src/App.tsx
+++ polyminal/src/App.tsx
@@ -1,36 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import shapes from "./shapes.json"
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
+  console.log("halo testing");
+  
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-black">
-        Hello world!
-      </h1>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main className="container mx-auto py-38 px-8">
+        <div className="grid lg:grid-cols-5 gap-6">
+          {shapes.map((shape) => (
+            <div className="shadow-lg rounded-lg justify-item-center p-5" key={shape.id}>
+              <img className="border border-black p-5" src="polyminal\src\img\cone.jpeg" alt="" />
+              <h5 className="p-3 text-xl font-bold flex justify-center">{shape.name}</h5>
+            </div>
+          ))}
+        </div>
+      </main>
     </>
   )
 }
