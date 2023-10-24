2eca2407d4ccc7a64a8019ed84d61691aade8b2c
MatthewMunthe <munthematthew115@gmail.com>
2023-10-24 14:59:13 +0700

    add ShapeCard

--- polyminal/src/App.tsx
+++ polyminal/src/App.tsx
@@ -2,22 +2,24 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import shapes from "./shapes.json"
+import ShapeCard from "./components/ShapeCard.tsx"
+import dataShape from "./shapes.json"
 import './App.css'
 
 function App() {
   // const [count, setCount] = useState(0)
   console.log("halo testing");
-  
 
   return (
     <>
       <main className="container mx-auto py-38 px-8">
         <div className="grid lg:grid-cols-5 gap-6">
           {shapes.map((shape) => (
-            <div className="shadow-lg rounded-lg justify-item-center p-5" key={shape.id}>
-              <img className="border border-black p-5" src="polyminal\src\img\cone.jpeg" alt="" />
-              <h5 className="p-3 text-xl font-bold flex justify-center">{shape.name}</h5>
-            </div>
+          //   <div className="shadow-lg rounded-lg justify-item-center p-5" key={shape.id}>
+          //   <img className="border border-black p-5" src="./img/cone.jpeg" alt="" />
+          //   <h5 className="p-3 text-xl font-bold flex justify-center">{shape.name}</h5>
+          // </div>
+          <ShapeCard />
           ))}
         </div>
       </main>
---
+++ polyminal/src/components/ShapeCard.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ShapeCard() {
+  return (
+    <div>shapeCard</div>
+  )
+}
+
+export default ShapeCard
