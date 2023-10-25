7b3ae953103509f56b9f553fea903ad8a31aa262
MatthewMunthe <munthematthew115@gmail.com>
2023-10-25 21:41:44 +0700

    pass json to ShapeCard

--- polyminal/src/App.tsx
+++ polyminal/src/App.tsx
@@ -3,7 +3,6 @@
 // import viteLogo from '/vite.svg'
 import shapes from "./shapes.json"
 import ShapeCard from "./components/ShapeCard.tsx"
-import dataShape from "./shapes.json"
 import './App.css'
 
 function App() {
@@ -15,11 +14,11 @@ function App() {
       <main className="container mx-auto py-38 px-8">
         <div className="grid lg:grid-cols-5 gap-6">
           {shapes.map((shape) => (
-          //   <div className="shadow-lg rounded-lg justify-item-center p-5" key={shape.id}>
-          //   <img className="border border-black p-5" src="./img/cone.jpeg" alt="" />
-          //   <h5 className="p-3 text-xl font-bold flex justify-center">{shape.name}</h5>
-          // </div>
-          <ShapeCard />
+            // <div className="shadow-lg rounded-lg justify-item-center p-5" key={shape.id}>
+            //   <img className="border border-black p-5" src={shape.img} alt="" />
+            //   <h5 className="p-3 text-xl font-bold flex justify-center">{shape.name}</h5>
+            // </div>
+          <ShapeCard data={shape}/>
           ))}
         </div>
       </main>
--- polyminal/src/components/ShapeCard.tsx
+++ polyminal/src/components/ShapeCard.tsx
@@ -1,9 +1,24 @@
 import React from 'react'
 
-function ShapeCard() {
-  return (
-    <div>shapeCard</div>
-  )
+// function ShapeCard() {
+//   return (
+//     // <div>shapeCard</div>
+//     <div className="shadow-lg rounded-lg justify-item-center p-5" key={shape.id}>
+//       {/* <img className="border border-black p-5" src="./img/cone.jpeg" alt="" /> */}
+//       <h5 className="p-3 text-xl font-bold flex justify-center">{shape.name}</h5>
+//     </div>
+//   )
+// }
+
+const ShapeCard = (props: { data: { id: any; name: any; img: any }; }) => {
+    const {id, name, img} = props.data;
+
+    return (
+        <div className="shadow-lg rounded-lg justify-item-center p-5" key={id}>
+            <img className="border border-black p-5" src={img} alt="" />
+            <h5 className="p-3 text-xl font-bold flex justify-center">{name}</h5>
+        </div>
+    )
 }
 
 export default ShapeCard
