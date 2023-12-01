d5a4d521e3b4e40469374dad759b9c41ca3b5fe8
Abiyyu Galih Ghifari <92355182+Abigalih@users.noreply.github.com>
2023-12-01 22:42:40 +0700

    shape detail (blom dynamic)

---
+++ polyminal/src/components/ShapeDetail.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const ShapeDetail = () => {
+  return (
+    // <h1>Shape Detail</h1>
+    <section>
+        <div className="grid grid-cols-2 gap-6 p-10">
+            <img src="public/img/torus.png" alt="" className="md:he"/>
+            <div>
+                <h1 className="font-bold">Fun Fact</h1>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus ipsum
+                    rerum facilis sit a fugit, voluptatem iste quasi quisquam voluptatum!</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus ipsum
+                    rerum facilis sit a fugit, voluptatem iste quasi quisquam voluptatum!</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus ipsum
+                    rerum facilis sit a fugit, voluptatem iste quasi quisquam voluptatum!</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus ipsum
+                    rerum facilis sit a fugit, voluptatem iste quasi quisquam voluptatum!</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus ipsum
+                    rerum facilis sit a fugit, voluptatem iste quasi quisquam voluptatum!</p>
+            </div>
+            <div>
+                <h1 className="font-bold">Fun Fact</h1>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus ipsum
+                    rerum facilis sit a fugit, voluptatem iste quasi quisquam voluptatum!</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus ipsum
+                    rerum facilis sit a fugit, voluptatem iste quasi quisquam voluptatum!</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus ipsum
+                    rerum facilis sit a fugit, voluptatem iste quasi quisquam voluptatum!</p>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default ShapeDetail
--- polyminal/src/main.tsx
+++ polyminal/src/main.tsx
@@ -3,8 +3,29 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import ShapeDetail from './components/ShapeDetail.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/ShapeDetail",
+    element: <ShapeDetail />,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <RouterProvider router={router} />
+// );
